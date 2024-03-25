d20e2b908a19c03db6a6d275ed0086f9e0b115a7
DanielFreeman <415919075@qq.com>
2024-03-25 14:32:25 +0800

    chore

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://hueyond.run/api/',
-  baseURL: 'https://localhost/api/',
+  baseURL: 'https://hueyond.run/api/',
+  // baseURL: 'https://localhost/api/',
   headers: {
     token: '22/7/13/18/57',
   },
